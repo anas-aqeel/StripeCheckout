4efca68c7a1497636ba3b800aa55892f3f6d6adc
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-09-12 12:29:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { useState } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar setOpen={setOpen} />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/products" element={<Products />} />  
+          <Route path="/product/:id" element={<Product /> } />  
+        </Routes>
+      </BrowserRouter>
+      <Cart open={open} setOpen={setOpen} />
+      {/* <Footer/> */}
     </div>
   );
 }
