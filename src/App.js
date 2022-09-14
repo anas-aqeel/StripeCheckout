74f8d6e634b75cb7e73f86e4b63a71ef44d70645
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-09-14 00:15:53 -0700

    Route updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import GlobalContext from "./context/GlobalContext";
+import Checkout from "./components/Checkout";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/product/:id" element={<Product />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
         <Cart open={open} setOpen={setOpen} />
         </GlobalContext>
